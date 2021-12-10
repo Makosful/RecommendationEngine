bacfd3d205276061ce6072a7d5f2b4fca67aad76
Axl <malthe.schwartz@protonmail.com>
2021-12-10 14:14:34 +0100

    refactor: Make Query.GetArticles non-async

--- Rytme.Recommendation.Engine.WebApi/GraphQL/Query.cs
+++ Rytme.Recommendation.Engine.WebApi/GraphQL/Query.cs
@@ -1,37 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using Rytme.Recommendation.Engine.WebApi.Data;
 using Rytme.Recommendation.Engine.WebApi.Entities;
 
-namespace Rytme.Recommendation.Engine.WebApi.GraphQL
+namespace Rytme.Recommendation.Engine.WebApi.GraphQL;
+
+public class Query
 {
-    public class Query
+    private readonly IArticleRepository _repository;
+
+    public Query(IArticleRepository repository)
     {
-        private readonly IArticleRepository _repository;
+        _repository = repository;
+    }
 
-        public Query(IArticleRepository repository)
+    public IEnumerable<Article> GetArticles(CancellationToken token)
+    {
+        Console.WriteLine(token);
+        return new List<Article>
         {
-            _repository = repository;
-        }
-
-        public async Task<IEnumerable<Article>> GetArticles(CancellationToken token)
-        {
-            Console.WriteLine(token);
-            return new List<Article>
+            new()
             {
-                new()
-                {
-                    Id = 123456789,
-                    Title = "How to Not to"
-                },
-                new()
-                {
-                    Id = 234567891,
-                    Title = "12 Rules for Life"
-                }
-            };
-        }
+                Id = 123456789,
+                Title = "How to Not to"
+            },
+            new()
+            {
+                Id = 234567891,
+                Title = "12 Rules for Life"
+            }
+        };
     }
 }
